6eb63ddf400ebee0f2755d09710488d4ac36500b
sabinaTT <tairsbn@gmail.com>
2021-12-20 18:50:03 -0800

    question 3 is working

--- script.js
+++ script.js
@@ -17,12 +17,10 @@ const timesUp = "";
 // const question9 = "";
 // const question10 = "";
 let img1 = document.createElement("img1").src = "http://path/to/image";
-
-
-
-
 document.querySelector(".countdown").style.display = "none";
 document.querySelector(".question-box").style.display= "none";
+
+
 
 /* Instruction Button */
 // get the modal
@@ -35,17 +33,18 @@ instructionButton.onclick = function() {
     modal.style.display ="block";
 }
 // When the user clicks on <span> (x), close the modal
-window.onload = function() {
+// window.onload = function() {
 span.onclick = function() {
     modal.style.display = "none";
 }
-};
+// };
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display ="none";
     }
 };
+
 
 /* About Button */
 // get the modal
@@ -67,9 +66,6 @@ window.onclick = function(event) {
         modalAbt.style.display ="none";
     } 
 };
-
-
-/* Move the Title */
 
 
 /* Timer */
@@ -117,6 +113,7 @@ startBtn.addEventListener("click", timer);
 //     let a = Math.floor(Math.random() * 10) + 1;
 //     let b = Math.floor(Math.random() * 10) + 1;
 //     let op = ["*", "+", "/", "-"][Math.floor(Math.random() * 4)];
+//     let multiply = document.querySelector(".ask-question").innerText = `${randNum1} x ${randNum2} = ?`;
 //     return prompt(`How much is ${a} + ${b}?`) == eval (a + op + b);
 
 // }
@@ -182,59 +179,152 @@ startBtn.addEventListener("click", function() {
 // };
 
 
-let userInput = document.querySelector("input").innerText 
-console.log(userInput)
 
-
-
-if (userInput > 0 ) {
-    console.log("you're incorrect")
-}
 
 
 
 
 // the questions
 
+// let randNum1 = Math.floor(Math.random() * 12) + 1;
+// let randNum2 = Math.floor(Math.random() * 12) + 1;
+
+
+
+// question 1
+// function question1() {
+//     let multiply = document.querySelector(".ask-question");
+//     multiply.innerText = `${randNum1} x ${randNum2} = ?`;
+//     return randNum1 * randNum2; 
+// };
+// let ans1 = question1();
+// console.log("ans1", ans1)
+
+
+// question 2
+
+// let randNum1 = Math.floor(Math.random() * 12) + 1;
+// let randNum2 = Math.floor(Math.random() * 12) + 1;
+// let userInput = document.querySelector("#question-input").value;
+/*
+function question2() {
+    let add = document.querySelector(".ask-question").innerText = `${randNum1} + ${randNum2} = ?`;
+    // let userInput = document.querySelector("#question-input").value;
+    let answer2 = randNum1 + randNum2;
+    let tOf = true; 
+    console.log(userInput);
+    if (userInput != answer2) {
+        return tOf = false;
+    } else {
+        return tOf;
+    }
+        return tOf;    
+};
+
+
+let ans2 = question2();
+console.log(userInput)
+
+// if (ans2 == userInput) {
+//     console.log("heyyyyoooo")
+// }
+
+*/
+
 let randNum1 = Math.floor(Math.random() * 12) + 1;
 let randNum2 = Math.floor(Math.random() * 12) + 1;
-
-function question1() {
-    return randNum1 * randNum2;
-};
-let ans1 = question1();
+let userInput = document.querySelector("#question-input");
+// console.log(userInput).value
+let submitButton = document.querySelector("#question-submit");
 
 
-
-function question2() {
-    return randNum1 + randNum2;
-};
-let ans2 = question2();
+// question 3
 function question3() {
+    let subtract = document.querySelector(".ask-question")
+    subtract.innerText = `${randNum1} - ${randNum2} = ?`;
     return randNum1 - randNum2;
 };
 let ans3 = question3();
+document.querySelector(".question-box").addEventListener("submit", function(e) {
+    e.preventDefault();
+    if (ans3 == userInput.value) {
+        console.log("correct!")
+    } else {
+        console.log('try again')
+    }
+    console.log(userInput.value)
+})
+
+
+
+
+ 
+
+
+
+/*
+// question 4
 function question4() {
+    let subtract = document.querySelector(".ask-question").innerText = `${randNum1} - ${randNum2} = ?`;
     return randNum1 - randNum2;
 };
 let ans4 = question4();
+
+
+// question 5
 function question5() {
+    let multiply = document.querySelector(".ask-question").innerText = `${randNum1} x ${randNum2} = ?`;
     return randNum1 * randNum2;
 };
 let ans5 = question5();
+
+// question 6
 function question6() {
+    let add = document.querySelector(".ask-question").innerText = `${randNum1} + ${randNum2} = ?`;
     return randNum1 + randNum2;
 };
 let ans6 = question6();
+ 
+
+*/
+
+// function ask() {
 
 
-function multiply() {
-document.querySelector("")
-let multiply = document.querySelector(".ask-question").innerText = `${randNum1} x ${randNum2} = ?`;
+//     return prompt(`How much is ${a} + ${b}?`) == eval (a + op + b);
+
+// }
+
+
+
+// console.log(correct)
+/*
+let a = Math.floor(Math.random() * 10) + 1;
+let b = Math.floor(Math.random() * 10) + 1;
+let op = ["*", "+", "-"][Math.floor(Math.random() * 3)];
+
+
+//     return prompt(`How much is ${a} + ${b}?`) == eval (a + op + b);
+
+// }
+//     let questions = [ask(), ask(), ask(), ask(), ask(), ask(), ask()],
+//         total = questions.length,
+//         correct = questions.filter(Boolean).length;
+
+function ask() {
+    let questionBox = document.querySelector(".ask-question")
+    questionBox.innerText = `${a} ${op} ${b} = ?`;
+    // return (a + op + b);
+    
+    // let questions = [ask(), ask(), ask(), ask(), ask(), ask(), ask()],
+    //     total = questions.length,
+    //     correct = questions.filter(Boolean).length;
+
+    // // if (userInput == )
+    console.log(`What is ${a} ${op} ${b}?`)
 }
+let ans1 = ask()
+// console.log(eval(userInput) === (ans1))
 
-let add = document.querySelector(".ask-question").innerText = `${randNum1} x ${randNum2} = ?`;
-let subtract = document.querySelector(".ask-question").innerText = `${randNum1} x ${randNum2} = ?`;
+*/
 
-
-console.log(ans6)   
